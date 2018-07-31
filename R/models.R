fe38c14e44b3c3c8bcf375f6a3b984724a81542b
Sergei Volkov <sevaspb@gmail.com>
2018-07-31 21:31:01 +0300

    Update models.R

--- R/models.R
+++ R/models.R
@@ -237,7 +237,7 @@ model_type.keras.engine.training.Model <- function(x, ...) {
   if (!requireNamespace('keras', quietly = TRUE)) {
     stop('The keras package is required for predicting keras models')
   }
-  print(keras::get_layer(x, index = -1)$activation)
+  print(keras::get_layer(x, index = -1)$activation$name)
   if (keras::get_layer(x, index = -1)$activation == 'linear') {
     'regression'
   } else {
